b15ac3cd3a841249f4903765dc9f41d35a43847e
Michael Stiles <mstiles01@icloud.com>
2019-12-09 19:34:27 -0600

    setup before Migration

---
+++ Backlog/Backlog/Models/MovieList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backlog.Models
+{
+    public class MovieList
+    {
+        public int MovieListId { get; set; }
+        public int MovieId { get; set; }
+        public int ListId { get; set; }
+        public Movie Movie { get; set; }
+        public List List { get; set;  }
+    }
+}
