4c0d7748dc64a5b8272353cf4cf91500d22f0643
Michael Stiles <mstiles01@icloud.com>
2019-12-09 20:09:16 -0600

    resetup files

---
+++ BacklogActual/BacklogActual/Models/ApplicationUser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BacklogActual.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BacklogActual.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+
+        }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+
+        [Required]
+        public string CityState { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+
+        public virtual ICollection<List> Lists { get; set; }
+
+        public virtual ICollection<MovieList> MovieLists { get; set; }
+    }
+}
---
+++ BacklogActual/BacklogActual/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace BacklogActual.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime PublishedDate { get; set; }
+        public ApplicationUser UserId { get; set; }
+
+
+    }
+}
